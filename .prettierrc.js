68897f9a532536e85fe1ff747fedd53966177861
Sarthak <sarthakbh321@gmail.com>
2021-06-24 18:17:19 +0530

    init

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  bracketSpacing: false,
+  jsxBracketSameLine: true,
+  singleQuote: false,
+  trailingComma: "all",
+  arrowParens: "avoid",
+  tabWidth: 4,
+  useTabs: true,
+};
---
+++ App.js
@@ -0,0 +1,16 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from "react";
+import {StyleSheet} from "react-native";
+
+const App = () => {};
+
+const styles = StyleSheet.create({});
+
+export default App;
